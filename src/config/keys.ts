37315f97e713a92fff16555814ec54009de26836
AkanWealth Asanga <akan.asanga@gmail.com>
2022-05-04 11:49:04 +0100

    env changes

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,5 @@
+import { config } from 'dotenv';
+config();
 export default {
-  mongoURI:
-    'mongodb+srv://akanwealth:XuQpSbke6Zd9ngJ@cluster0.c0f9b.mongodb.net/nest-test?retryWrites=true&w=majority',
+  mongoURI: process.env.URI,
 };
